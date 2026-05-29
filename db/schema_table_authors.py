589658ac8262d0462a5f70304728f15680852193
esch <escher@berkeley.edu>
2026-05-29 17:52:45 +0200

    added the table schemas for authors and labels

---
+++ db/schema_table_authors.py
@@ -0,0 +1,9 @@
+SCHEMA_AUTHORS = """
+    author_id TEXT PRIMARY KEY,
+    short_id TEXT,
+    name TEXT,
+    display_name TEXT,
+    email TEXT,
+    name_normalized TEXT,
+    last_modified TEXT
+"""
---
+++ db/schema_table_labels.py
@@ -0,0 +1,19 @@
+from config.config_db import TABLE_LABELS
+from db.db_query_utils import query_db_results
+
+SCHEMA_LABELS = """
+    id TEXT PRIMARY KEY,
+    label TEXT,
+    space_id TEXT,
+    retrieved_at TEXT,
+"""
+
+def get_labels_cache():
+    rows = query_db_results("label", TABLE_LABELS)
+    labels = [row[0] for row in rows]
+    return set(labels)      # remove duplicates
+
+def get_all_labels_with_ids():
+    results = query_db_results("id, label", TABLE_LABELS)
+    return [{"id": r[0], "label": r[1]} for r in results]
+
