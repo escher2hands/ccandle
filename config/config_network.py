d35a520e6bf85f97057f8789046af8be464a7ee2
esch <escher@berkeley.edu>
2026-05-29 17:53:10 +0200

    added option for using Confluence Cloud v1 APIs

--- config/config_network.py
+++ config/config_network.py
@@ -1,6 +1,10 @@
 from config.confluence_auth import load_conf_url
 
-CONFLUENCE_BASE_URL = load_conf_url()       # as users may have a different confluence instance
+API_V2 = "wiki/api/v2/"
+API_V1 = "wiki/rest/api/"
+
+CONFLUENCE_BASE_URL = load_conf_url() + API_V2       # as users may have a different confluence instance
+CONFLUENCE_BASE_URL_V1 = load_conf_url() + API_V1       # as users may have a different confluence instance
 DEFAULT_HEADERS = { "Accept": "application/json" }
 
 ENDPOINT_PAGES = "pages"
