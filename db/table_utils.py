cb7f87f0c1bd66ef0b5a3f7e0511f944ae3f8232
esch <escher@berkeley.edu>
2026-05-29 17:50:51 +0200

    refactored table creation and dropping to its own utility

---
+++ db/table_utils.py
@@ -0,0 +1,24 @@
+from config.config_db import PATH_DB
+import sqlite3
+
+def create_table(table_name, table_schema):
+    conn = sqlite3.connect(PATH_DB)
+    cur = conn.cursor()
+    cur.execute(f"""CREATE TABLE IF NOT EXISTS ? (?)""", (table_name, table_schema))
+    conn.commit()
+    conn.close()
+
+def create_table_hard(table_name, table_schema):
+    conn = sqlite3.connect(PATH_DB)
+    cur = conn.cursor()
+    cur.execute(f"""DROP TABLE IF EXISTS ?""", (table_name, ))
+    cur.execute(f"""CREATE TABLE IF NOT EXISTS ? (?)""", (table_name, table_schema))
+    conn.commit()
+    conn.close()
+
+def drop_table(table_name):
+    conn = sqlite3.connect(PATH_DB)
+    cur = conn.cursor()
+    cur.execute(f"""DROP TABLE IF EXISTS ?""", (table_name, ))
+    conn.commit()
+    conn.close()
