d11e3de6349cf9eb0246952f2babe71e9217de8d
esch <escher@berkeley.edu>
2026-06-01 00:01:17 +0200

    added tiny_link tracking for later. Updated valid fields list.

--- pages/schema_table_pages.py
+++ pages/schema_table_pages.py
@@ -7,6 +7,7 @@ SCHEMA_PAGES = """
     authors TEXT,
     space_id TEXT,
     html TEXT,
+    tiny_link TEXT,
     retrieved_at TEXT,
 
     plain_text TEXT,
@@ -31,6 +32,7 @@ SCHEMA_PAGES = """
     links_list TEXT,
     child_list TEXT,
     mentions_list TEXT,
+    duplicate_list TEXT,
     labels TEXT,
 
     llama_summary TEXT,
@@ -42,9 +44,9 @@ SCHEMA_PAGES = """
     processed_version INTEGER
 """
 
-VALID_FIELDS = ["title", "version", "last_modified", "authors", "space_id", "html", "retrieved_at",
+VALID_FIELDS = ["title", "version", "last_modified", "authors", "space_id", "html", "tiny_link", "retrieved_at",
     "plain_text", "lead_para", "eval_smell", "eval_summary", "word_count", "link_count", "image_count", "has_link_tree", "metrics_json",
     "page_type", "mm_smell", "rn_smell", "pt_smell", "ws_smell", "sd_smell", "ci_smell", "lp_smell",
-    "links_list","child_list", "mentions_list", "labels",
+    "links_list","child_list", "mentions_list", "duplicate_list", "labels",
     "llama_summary", "vector_embedding", "vector_reduced", "kw_fingerprint","similarity_cluster",]
 
