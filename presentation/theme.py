d99555287bb7a0f79febfe9d07a5cd5383ef85f7
esch <escher@berkeley.edu>
2026-05-31 12:53:05 +0200

    moved all coler definitions to theme.py

---
+++ presentation/theme.py
@@ -0,0 +1,12 @@
+
+# basic color definitions
+RED = "\033[1;31m"
+YELLOW = "\033[1;33m"
+GREEN = "\033[0;32m"
+BLUE = "\033[1;34m"
+RESET = "\033[0m"
+BOLD = "\033[1m"
+DIM = "\033[2m"
+
+# basic sizing
+WIDTH_NICE = 51
