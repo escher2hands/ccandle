39b6e55b7e96922f740bc34c4bcdc41b8cc57c6c
escher2hands <escher@berkeley.edu>
2026-06-19 00:28:04 +0200

    Support easy sql queries of db from cli (#5)
    * support sql queries from the terminal
    
    * support better formatting options for previews of table data

--- sanity/smoke_tester.py
+++ sanity/smoke_tester.py
@@ -31,15 +31,6 @@ def interactive_smoke_test():
         BASE_CMD + ["spaces", "list"],
         BASE_CMD + ["spaces", "list", "--filter", test_space_query],
         BASE_CMD + ["spaces", "configured"],
-
-        #BASE_CMD + ["sync"],
-        BASE_CMD + ["sync", "--from-step", "parse_text"],
-
-        BASE_CMD + ["labels", "add", "smoke-test-label", "2622718175", "2455404674"],
-        BASE_CMD + ["labels", "delete", "smoke-test-label", "2622718175", "2455404674"],
-
-        #BASE_CMD + ["sql", "select id, labels, title from pages limit 15"],
-        #BASE_CMD + ["sql", "columns"],
     ]
 
     return smoke_test(test_commands=TEST_COMMANDS)
