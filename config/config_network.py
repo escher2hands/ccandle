31a586dded709e3e35e23920e5e1ce71f956d0b9
morganrandall <morganrandall@hilti.com>
2026-06-29 22:14:36 +0200

    made a bit less finicky through forcing trailing slash.

--- config/config_network.py
+++ config/config_network.py
@@ -3,8 +3,9 @@ from config.confluence_auth import load_conf_url
 API_V2 = "wiki/api/v2/"
 API_V1 = "wiki/rest/api/"
 
-CONFLUENCE_BASE_URL = load_conf_url() + API_V2       # as users may have a different confluence instance
-CONFLUENCE_BASE_URL_V1 = load_conf_url() + API_V1       # as users may have a different confluence instance
+user_input_url = load_conf_url().rstrip('/') + '/'      # guard against issues with trailing slash
+CONFLUENCE_BASE_URL = user_input_url + API_V2
+CONFLUENCE_BASE_URL_V1 = user_input_url + API_V1
 DEFAULT_HEADERS = { "Accept": "application/json" }
 
 ENDPOINT_PAGES = "pages"
