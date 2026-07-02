803b7e8dde635b471a775a474ffdd4112f9e646e
morganrandall <morganrandall@hilti.com>
2026-07-02 23:32:31 +0200

    reformatted valid fields var to make checking against the schema easier.

--- pages/schema_table_pages.py
+++ pages/schema_table_pages.py
@@ -45,9 +45,19 @@ SCHEMA_PAGES = """
     processed_version INTEGER
 """
 
-VALID_FIELDS = ["title", "version", "last_modified", "authors", "space_id", "html", "tiny_link", "retrieved_at",
-    "plain_text", "lead_para", "eval_smell", "eval_summary", "word_count", "link_count", "image_count", "has_link_tree", "metrics_json",
+VALID_FIELDS = [
+    "id",
+
+    "title", "version", "last_modified", "authors", "space_id", "html", "tiny_link", "retrieved_at",
+
+    "plain_text", "lead_para", "eval_smell", "eval_summary", "word_count", "link_count", "image_count", "has_link_tree", "eval_notes",
+
     "page_type", "mm_smell", "rn_smell", "pt_smell", "ws_smell", "sd_smell", "ci_smell", "lp_smell",
-    "links_list","child_list", "mentions_list", "duplicate_list", "labels",
-    "llama_summary", "vector_embedding", "vector_reduced", "kw_fingerprint","similarity_cluster",]
+
+    "links_list", "child_list", "mentions_list", "duplicate_list", "excerpts", "labels",
+
+    "llama_summary", "vector_embedding", "vector_reduced", "kw_fingerprint", "similarity_cluster",
+
+    "processed_version",
+]
 
