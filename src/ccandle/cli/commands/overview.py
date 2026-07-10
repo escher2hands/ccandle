dd901e009a8b2240ecd50e16a523013a343a859a
morganrandall <morganrandall@hilti.com>
2026-07-10 12:24:23 +0200

    refactored for fuzzy space id, and enabled corpus-wide aggregation

--- src/ccandle/cli/commands/overview.py
+++ src/ccandle/cli/commands/overview.py
@@ -1,15 +1,20 @@
 # Fast overview of Confluence space quality, connectedness, and navigability.
+from ccandle.presentation.user_communication import clean_user_space_id_or_exit
+
 
 def register(subparsers):
     p = subparsers.add_parser("overview", help="Learn quick stats on your configured Confluence spaces")
-    p.add_argument("--space-id", help="Narrow overview to just one space")
+    p.add_argument("--space", help="Narrow overview to just one space")
     p.add_argument("--quiet", "-q", action="store_true", default=False,
                         help="Mute verbose explanations")
+    p.add_argument("--corpus", action="store_true", default=False,
+                        help="Get an overall combined evaluation across all spaces, instead of individual evaluations per space (total across population vs. per space)")
 
 def run(args):
     from ccandle.analysis.space_overview import present_all_space_overviews, present_space_overview
-    if args.space_id:
-        present_space_overview(space_id=args.space_id, quiet=args.quiet)
+    space_id = clean_user_space_id_or_exit(args.space)
+    if space_id or args.corpus:
+        present_space_overview(space_id=space_id, quiet=args.quiet)
     else:
         present_all_space_overviews(quiet=args.quiet)
     return 0
