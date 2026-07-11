9a15bc3b88cc1dd37084a2040a34c5b5449d47ed
morganrandall <morganrandall@hilti.com>
2026-07-11 15:47:35 +0200

    closed some edge cases in some CLI commands.

--- src/ccandle/cli/commands/sync.py
+++ src/ccandle/cli/commands/sync.py
@@ -1,7 +1,7 @@
 # Sync: store an offline copy of page data from Confluence
 # optionally restart from a set step
 # optionally hard refresh
-from ccandle.sync.sync_all import VALID_STEPS
+from ccandle.sync.sync_all import VALID_STEPS, API_STEPS
 
 def register(subparsers):
     p = subparsers.add_parser("sync", help="Sync pages from your Confluence Cloud to your local database in ccandle")
@@ -12,7 +12,7 @@ def register(subparsers):
 def run(args):
     from ccandle.sync.sync_all import sync
     from ccandle.network.network_utils import check_network_connection
-    if args.from_step in ["children", "authors", "labels", None]:
+    if args.from_step in API_STEPS + [None]:
         if not check_network_connection():
             return 1                # force internet connection for steps that require API connection
     sync(args.hard_refresh, args.from_step)
