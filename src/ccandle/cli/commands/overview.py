836e654062957b2967797b93c0c8c61b255f150f
morganrandall <morganrandall@hilti.com>
2026-07-12 15:19:28 +0200

    output json format instead of human pretty printing

--- src/ccandle/cli/commands/overview.py
+++ src/ccandle/cli/commands/overview.py
@@ -1,4 +1,6 @@
 # Fast overview of Confluence space quality, connectedness, and navigability.
+import json
+
 from ccandle.presentation.user_communication import clean_user_space_id_or_exit
 
 
@@ -9,13 +11,17 @@ def register(subparsers):
                         help="Mute verbose explanations")
     p.add_argument("--corpus", action="store_true", default=False,
                         help="Get an overall combined evaluation across all spaces, instead of individual evaluations per space (total across population vs. per space)")
+    p.add_argument("--json", action="store_true", default=False, help="Output in easy parseable JSON format")
 
 def run(args):
     from ccandle.analysis.space_overview import present_all_space_overviews, present_space_overview
     space_id = clean_user_space_id_or_exit(args.space)
     if space_id or args.corpus:
-        present_space_overview(space_id=space_id, quiet=args.quiet)
+        results = present_space_overview(space_id=space_id, quiet=args.quiet, json_format=args.json)
     else:
-        present_all_space_overviews(quiet=args.quiet)
+        results = present_all_space_overviews(quiet=args.quiet, json_format=args.json)
+
+    if args.json:
+        print(json.dumps(results, indent=2))
     return 0
 
