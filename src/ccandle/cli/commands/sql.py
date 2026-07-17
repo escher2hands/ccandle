45f1939a6ef0571c6abaebe894fbfadeaecc6fca
morganrandall <morganrandall@hilti.com>
2026-07-18 01:41:30 +0200

    refactored to move printing into the cli, add clickable url rendering.

--- src/ccandle/cli/commands/sql.py
+++ src/ccandle/cli/commands/sql.py
@@ -1,5 +1,9 @@
-# surface db access via sql to users through the CLI.
+# expose the db directly to users via the CLI.
+# - ccandle sql columns
+# - ccandle sql query QUERY
+from ccandle.spaces.space_utils import get_space_attribute
 from ccandle.config.config_db import TABLE_PAGES, TABLE_LIST
+from ccandle.db.db_utils import get_field_in_pages
 from ccandle.presentation.theme import *
 
 def register(subparsers):
@@ -9,6 +13,7 @@ def register(subparsers):
     query_p = sql_sub.add_parser("query", help="Run a SQL query against your local pages db")
     query_p.add_argument("query", help="Your SQL query string")
     query_p.add_argument("--force-table", action="store_true", help="Force table view, truncating lengthy values")
+    query_p.add_argument("--clickable", action="store_true", help="Append a clickable url to the results")
 
     col_sub = sql_sub.add_parser("columns", help="List the columns in your local pages table")
     col_sub.add_argument("--table", default=TABLE_PAGES, help="Your SQL query string")
@@ -17,6 +22,8 @@ def register(subparsers):
 def run(args):
     from ccandle.analysis.sql_queries import query_via_cli, get_column_names
     from ccandle.config.config_app import APP_HANDLE
+    from ccandle.presentation.page_previews import render_results
+    from ccandle.config.confluence_auth import load_conf_url
 
     if args.sql_cmd == "columns":
         columns = get_column_names(your_table=args.table)
@@ -31,7 +38,21 @@ def run(args):
              f"{DIM}as they have specific data you may want to merge.{RESET}")
         return 0
     elif args.sql_cmd == "query":
-        return query_via_cli(args.query, force_table=args.force_table)
+        results, columns = query_via_cli(args.query)
+        if args.clickable:
+            if not any(col["key"] == "id" for col in columns):
+                print(f"{RED}" + "-" * WIDTH_NICE + "\n"
+                      f"There is no id field in your sql query.\n"
+                      f"{DIM}The {RESET}--clickable{RED}{DIM} flag requires you to include {RESET}'id'{RED}{DIM} in your select clause.\n"
+                      f"Otherwise, {APP_HANDLE} cannot generate clickable links in query output.{RESET}")
+                return 1
+            columns.append({"key": "link", "label": "LINK"})
+            for res in results:
+                tiny_link = get_field_in_pages(res['id'], "tiny_link")
+                res['link'] = f"{load_conf_url()}/wiki{tiny_link}/"
+
+        render_results(results, columns, force_table=args.force_table)
+        return 0
     else:
         print(f"{RED}You must choose a command.\n"
               f"{DIM}Use: \n"
