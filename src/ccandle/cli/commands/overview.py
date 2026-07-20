47af0b1fd2fe412ea509404ddfb0d1c89779a210
morganrandall <morganrandall@hilti.com>
2026-07-20 15:06:27 +0200

    split overview generation from presentation, to better facilitate reuse.

--- src/ccandle/cli/commands/overview.py
+++ src/ccandle/cli/commands/overview.py
@@ -14,14 +14,17 @@ def register(subparsers):
     p.add_argument("--json", action="store_true", default=False, help="Output in easy parseable JSON format")
 
 def run(args):
-    from ccandle.analysis.space_overview import present_all_space_overviews, present_space_overview
+    from ccandle.overview.generate_space_overview import generate_all_space_overviews, generate_space_overview
+    from ccandle.overview.present_space_overview import print_space_overview
     space_id = clean_user_space_id_or_exit(args.space)
     if space_id or args.corpus:
-        results = present_space_overview(space_id=space_id, quiet=args.quiet, json_format=args.json)
+        results = [generate_space_overview(space_id=space_id)]
     else:
-        results = present_all_space_overviews(quiet=args.quiet, json_format=args.json)
+        results = generate_all_space_overviews()
 
     if args.json:
         print(json.dumps(results, indent=2))
+    else:
+        print_space_overview(results)
     return 0
 
