a70067640f17424318335e236bbdc78f32f151ca
esch <escher@berkeley.edu>
2026-07-27 12:03:18 +0200

    slightly changed formatting, and included the autocreation of artifact dir in the config_db.py file.

--- src/ccandle/config/config_db.py
+++ src/ccandle/config/config_db.py
@@ -3,21 +3,24 @@ from platformdirs import user_data_dir
 from pathlib import Path
 from importlib.resources import files
 
-TABLE_PAGES = "pages"
-TABLE_LABELS = "labels"
-TABLE_AUTHORS = "authors"
-TABLE_VECTORS = "vectors"
-TABLE_LIST = [TABLE_PAGES, TABLE_LABELS, TABLE_AUTHORS, TABLE_VECTORS]
+# ——— TABLES ———————————————————————————————————
+TABLE_PAGES     = "pages"
+TABLE_LABELS    = "labels"
+TABLE_AUTHORS   = "authors"
+TABLE_VECTORS   = "vectors"
+TABLE_LIST      = [TABLE_PAGES, TABLE_LABELS, TABLE_AUTHORS, TABLE_VECTORS]
 
-DATA_DIR = Path(user_data_dir(APP_NAME))
-DB_DIR = DATA_DIR / "db"
-CONFIG_DIR = DATA_DIR / "config"
-ARTIFACT_DIR = DATA_DIR / "artifacts"
+# ——— PATHS & DIRS —————————————————————————————
+DATA_DIR            = Path(user_data_dir(APP_NAME))
+DB_DIR              = DATA_DIR / "db"
+CONFIG_DIR          = DATA_DIR / "config"
+ARTIFACT_DIR        = DATA_DIR / "artifacts"
 
-PATH_SPACES_CONFIG = CONFIG_DIR / "config_spaces.json"
-PATH_DB = DB_DIR / "confluence_mirror.db"
-PATH_MODEL = CONFIG_DIR / "type_model.joblib"
-for directory in (DB_DIR, CONFIG_DIR):
+PATH_DB             = DB_DIR / "confluence_mirror.db"
+PATH_SPACES_CONFIG  = CONFIG_DIR / "config_spaces.json"
+PATH_MODEL          = CONFIG_DIR / "type_model.joblib"
+
+for directory in (DB_DIR, CONFIG_DIR, ARTIFACT_DIR):
     directory.mkdir(parents=True, exist_ok=True)
 
 PATH_BUNDLED_MODEL = files("ccandle.artifacts").joinpath("type_model.joblib") # the default location when src is cloned
