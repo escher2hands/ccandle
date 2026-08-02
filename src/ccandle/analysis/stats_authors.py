ebafa4e7a135a511adce54a61b61dfd9144077c4
esch <escher@berkeley.edu>
2026-08-02 19:06:26 +0200

    enhanced author edit counts with count of unique pages edited, and total authors in the query.

--- src/ccandle/analysis/stats_authors.py
+++ src/ccandle/analysis/stats_authors.py
@@ -6,31 +6,40 @@ import sqlite3, json
 def find_top_authors_across_pages(pid_list=None, limit=50, space_id=None, path_to_db=PATH_DB):
     if pid_list is None:
         pid_list = get_all_ids_in_pages(path_to_db=path_to_db)
-    author_counter = Counter()
+
+    author_edit_counter = Counter()
+    author_page_counter = Counter()
+
     space_id_query = f"space_id = {space_id}" if space_id else "1=1"
 
     with sqlite3.connect(path_to_db) as conn:
         cur = conn.cursor()
 
-        for pid_batch in _chunked(pid_list, 500):            # I think sql complains if batches are larger than 999
+        for pid_batch in _chunked(pid_list, 500):
             placeholders = ",".join("?" * len(pid_batch))
-            query = f"""SELECT authors FROM {TABLE_PAGES}
-                    WHERE id IN ({placeholders}) AND {space_id_query}"""
-
+            query = f"""
+                SELECT authors
+                FROM {TABLE_PAGES}
+                WHERE id IN ({placeholders}) AND {space_id_query}
+            """
             cur.execute(query, pid_batch)
 
-            author_counter.update(
-                author
-                for (authors_json,) in cur
-                for author in json.loads(authors_json)
-                # if author != "DELETED"                    # let's see if we get away with leaving these deleteds here
-            )
+            for (authors_json,) in cur:
+                authors = json.loads(authors_json)
+                author_edit_counter.update(authors)         # Count every edit event
+                author_page_counter.update(set(authors))    # Count each author once per page
 
-    top_authors = author_counter.most_common(limit)
-    return [
-        {"name": name_normalized, "edits": edits}
-        for (name_normalized, edits) in top_authors
-    ]
+    return {
+        "unique_authors": len(author_edit_counter),
+        "results": [
+            {
+                "name": author,
+                "edits": edits,
+                "pages": author_page_counter[author],
+            }
+            for author, edits in author_edit_counter.most_common(limit)
+        ],
+    }
 
 
 def _chunked(iterable, size):
