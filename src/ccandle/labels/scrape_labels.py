8c0e3aed864ea3bb3125aa54046b021ed03a76f0
esch <escher@berkeley.edu>
2026-09-25 15:19:20 +0200

    cast to iso format, added intro comment.

--- src/ccandle/labels/scrape_labels.py
+++ src/ccandle/labels/scrape_labels.py
@@ -1,36 +1,46 @@
+# Create a local 'shadow' of the labels (user metadata) of pages in Confluence.
+#
+# Confluence's default label handling is like pulling teeth when working in bulk.
+# Correcting obnoardig to onboarding after it's been erroneously added to 15 pages
+# is possible via automation scripts...but why not include some bulk label
+# correction code in ccandle? Additionally, sql queries are better if you can
+# hook onto existing user created metadata--which are exactly labels in Confluence.
+# This module enables both, by scraping labels from the Confluence server,
+# maintaining a mapping to pages and spaces, etc.
+
 from tqdm import tqdm
 from ccandle.spaces.space_utils import list_configured_space_ids
 from ccandle.labels.schema_table_labels import get_all_labels_with_ids
 from ccandle.db.table_utils import create_table_hard
 from ccandle.network.network_utils import request_labels_for_space, request_pages_for_label
+from ccandle.config.config_db import TABLE_LABELS, PATH_DB, TABLE_PAGES
+from ccandle.labels.schema_table_labels import SCHEMA_LABELS
+from ccandle.config.config_app import FRIENDLY_APP_NAME
+import json
+import sqlite3
 import datetime
 from collections import defaultdict
 
 def scrape_labels(quiet=False):
-    from ccandle.config.config_app import FRIENDLY_APP_NAME
     print(f"Syncing labels from your Confluence spaces to {FRIENDLY_APP_NAME}...")
-    all_labels = sync_label_names_from_confluence()
+    all_labels = _sync_label_names_from_confluence()
     print(f"Done. {FRIENDLY_APP_NAME} has synced all {len(all_labels)} labels from your Confluence spaces..")
 
     print("\nGetting page data for each label...")
-    pages_with_labels = sync_labels_to_pages(quiet=quiet)
+    pages_with_labels = _sync_labels_to_pages(quiet=quiet)
     print(f"Done. Updated {len(pages_with_labels)} pages with their labels.")
 
-def sync_label_names_from_confluence():
-    time_stamp = datetime.datetime.now(datetime.timezone.utc)
+def _sync_label_names_from_confluence():
+    time_stamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
     all_labels = [
         {"id": label["id"], "label": label["label"], "space_id": space_id, "retrieved_at": time_stamp}
         for space_id in list_configured_space_ids()
         for label in request_labels_for_space(space_id)   # expensive API call. Luckily, most users don't track many spaces
     ]
-    store_synced_labels(all_labels)
+    _store_synced_labels(all_labels)
     return all_labels
 
-def store_synced_labels(freshly_synced_label_records):
-    import sqlite3
-    from ccandle.config.config_db import TABLE_LABELS, PATH_DB
-    from ccandle.labels.schema_table_labels import SCHEMA_LABELS
-
+def _store_synced_labels(freshly_synced_label_records):
     create_table_hard(TABLE_LABELS, SCHEMA_LABELS)        # kill whatever table already existed, and build a new one.
 
     records = [
@@ -54,7 +64,7 @@ def store_synced_labels(freshly_synced_label_records):
     conn.close()
 
 
-def sync_labels_to_pages(quiet=False):
+def _sync_labels_to_pages(quiet=False):
     label_records = get_all_labels_with_ids()
     page_to_labels = defaultdict(set)                         # ensure no duplicates
 
@@ -63,14 +73,11 @@ def sync_labels_to_pages(quiet=False):
         for pid in pids:
             page_to_labels[pid].add(label_rec["label"])
 
-    store_page_label_mapping(page_to_labels)
+    _store_page_label_mapping(page_to_labels)
     return page_to_labels
 
 
-def store_page_label_mapping(page_to_labels):
-    import sqlite3, json
-    from ccandle.config.config_db import PATH_DB, TABLE_PAGES
-
+def _store_page_label_mapping(page_to_labels):
     with sqlite3.connect(PATH_DB) as conn:
         cur = conn.cursor()
         cur.execute(f"""UPDATE {TABLE_PAGES} SET labels = '[]'""") # clear all existing labels
